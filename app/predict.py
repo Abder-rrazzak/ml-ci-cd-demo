c306d81e75375e63424770ca09d4321144d3a8d7
Abder-rrazzak <115995720+Abder-rrazzak@users.noreply.github.com>
2025-09-25 20:30:53 +0100

    predict.py

--- app/predict.py
+++ app/predict.py
@@ -1,7 +1,19 @@
+import os
 import joblib
 import numpy as np
+from app.model import train_and_save_model
 
-model = joblib.load("model.joblib")
+MODEL_PATH = "model.joblib"
+
+# Vérifie si le modèle existe, sinon le crée
+if not os.path.exists(MODEL_PATH):
+    print("✅ Modèle non trouvé. Entraînement en cours...")
+    train_and_save_model()
+else:
+    print("✅ Modèle chargé depuis le disque.")
+
+# Charge le modèle une fois qu’il existe
+model = joblib.load(MODEL_PATH)
 
 def predict(data):
     return model.predict(np.array(data).reshape(1, -1)).tolist()
