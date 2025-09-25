8f8c777ccb0f7bbf350892a3a6ff389f372e596b
Abder-rrazzak <115995720+Abder-rrazzak@users.noreply.github.com>
2025-09-25 20:55:22 +0100

    test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,7 +3,14 @@ from app.api import app
 
 client = TestClient(app)
 
-def test_api_predict():
-    response = client.post("/predict", json={"features": [5.1, 3.5, 1.4, 0.2]})
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "message" in response.json()
+
+def test_predict_endpoint():
+    data = {"features": [5.1, 3.5, 1.4, 0.2]}
+    response = client.post("/predict", json=data)
     assert response.status_code == 200
     assert "prediction" in response.json()
+    assert isinstance(response.json()["prediction"], list)
