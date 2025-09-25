376c9927567d8f587dea271ee37e3bcfd037105d
Abder-rrazzak <115995720+Abder-rrazzak@users.noreply.github.com>
2025-09-25 20:21:46 +0100

    test_predict.py

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -2,6 +2,13 @@ import os
 import joblib
 import numpy as np
 from app.model import train_and_save_model
+from app.predict import predict
+
+def test_prediction_output():
+    sample = [5.1, 3.5, 1.4, 0.2]
+    result = predict(sample)
+    assert isinstance(result, list)
+    assert len(result) == 1
 
 MODEL_PATH = "model.joblib"
 
